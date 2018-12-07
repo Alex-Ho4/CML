3c81be6381adfa7cd04cac52d6a83dddfa33843b
Alex Ho <alextho4@gmail.com>
2018-12-07 04:06:57 -0600

    updated test.txt

--- time_test.c
+++ time_test.c
@@ -18,7 +18,7 @@ double diffTime ( struct timespec t0, struct timespec t1 )
 
 int main ( int argc, char* argv[] )
 {
-  //custom _malloc
+  //_malloc
   {
     char* A = NULL;
     long long unsigned int t0;
@@ -44,19 +44,22 @@ int main ( int argc, char* argv[] )
     _free(A);
   }
 
-  //normal _malloc
+  //_free
   {
-    char* A = NULL;
+    char* A[100];
     long long unsigned int t0;
     long long unsigned int t1;
     long long unsigned int min = 0xfffffff; // a big number
     long long unsigned int max = 0.0;       // a small number
     // ignore the time on the first one
-    A = (char*) malloc (10);
+    for (int i=0;i<100;i++)
+    {
+      A[i] = (char*) _malloc (10);
+    }
     for (int i=0;i<100;i++)
     {
       t0 = rdtsc ();
-      A = (char*) malloc (10);
+      _free(A[i]);
       t1 = rdtsc ();
       unsigned long long int dt = t1 - t0;
       // might want to print dt out and graph it
@@ -67,7 +70,62 @@ int main ( int argc, char* argv[] )
         max = dt;
     }
     printf ( "min = %lld max = %lld \n", min, max );
-    free(A);
+    _free((char*)A[0]);
+  }
+
+  //_calloc
+  {
+    char* A = NULL;
+    long long unsigned int t0;
+    long long unsigned int t1;
+    long long unsigned int min = 0xfffffff; // a big number
+    long long unsigned int max = 0.0;       // a small number
+    // ignore the time on the first one
+    A = (char*) _calloc (1,10);
+    for (int i=0;i<100;i++)
+    {
+       t0 = rdtsc ();
+       A = (char*) _calloc (1,10);
+       t1 = rdtsc ();
+       unsigned long long int dt = t1 - t0;
+       // might want to print dt out and graph it
+       printf (" %lld\n", dt );
+       if (dt < min)
+         min = dt;
+       if (dt >max)
+         max = dt;
+    }
+    printf ( "min = %lld max = %lld \n", min, max );
+    _free(A);
+  }
+
+  //_realloc
+  {
+    char* A[100];
+    long long unsigned int t0;
+    long long unsigned int t1;
+    long long unsigned int min = 0xfffffff; // a big number
+    long long unsigned int max = 0.0;       // a small number
+    // ignore the time on the first one
+    for (int i=0;i<100;i++)
+    {
+      A[i] = (char*) _malloc (10);
+    }
+    for (int i=0;i<100;i++)
+    {
+       t0 = rdtsc ();
+       A[i] = (char*) _realloc ((char*)A[i], 20);
+       t1 = rdtsc ();
+       unsigned long long int dt = t1 - t0;
+       // might want to print dt out and graph it
+       printf (" %lld\n", dt );
+       if (dt < min)
+         min = dt;
+       if (dt >max)
+         max = dt;
+    }
+    printf ( "min = %lld max = %lld \n", min, max );
+    _free((char*)A[0]);
   }
   return 0;
 }
